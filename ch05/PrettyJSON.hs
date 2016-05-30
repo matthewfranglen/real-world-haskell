982056e5338bdae07ccdce37e76d3c2b0b801af0
Matthew Franglen <matthew@franglen.org>
2016-05-30 21:23:54 +0100

    More breaking changes

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -1,17 +1,18 @@
 module PrettyJSON (renderJValue) where
 
 import Numeric (showHex)
-import Data.Bits (shiftR, (.&.))
 import Data.Char (ord)
+import Data.Bits (shiftR, (.&.))
+
 import SimpleJSON (JValue(..))
-import Prettify
+import Prettify (Doc, (<>), char, double, fsep, hcat, punctuate, text, compact, pretty)
 
 renderJValue :: JValue -> Doc
 renderJValue (JBool True)  = text "true"
 renderJValue (JBool False) = text "false"
 renderJValue JNull         = text "null"
 renderJValue (JNumber num) = double num
-renderJValue (JString str) = PrettyJSON.string str
+renderJValue (JString str) = string str
 renderJValue (JArray ary)  = series '[' ']' renderJValue ary
 renderJValue (JObject obj) = series '{' '}' field obj
     where field (name,val) = string name
