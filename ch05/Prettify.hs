8d48b950bdea5af6280a1049cf4013c43851c943
Matthew Franglen <matthew@franglen.org>
2016-05-30 21:14:50 +0100

    Fix location of definitions

--- ch05/Prettify.hs
+++ ch05/Prettify.hs
@@ -14,5 +14,19 @@ text str = undefined
 double :: Double -> Doc
 double num = undefined
 
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
 fsep :: [Doc] -> Doc
 fsep xs = undefined
+
+punctuate :: Doc -> [Doc] -> [Doc]
+punctuate p []     = []
+punctuate p [d]    = [d]
+punctuate p (d:ds) = (d <> p) : punctuate p ds
--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -17,15 +17,6 @@ string = enclose '"' '"' . hcat . map oneChar
 enclose :: Char -> Char -> Doc -> Doc
 enclose left right x = char left <> x <> char right
 
-(<>) :: Doc -> Doc -> Doc
-a <> b = undefined
-
-char :: Char -> Doc
-char c = undefined
-
-hcat :: [Doc] -> Doc
-hcat xs = undefined
-
 oneChar :: Char -> Doc
 oneChar c = case lookup c simpleEscapes of
               Just r -> text r
