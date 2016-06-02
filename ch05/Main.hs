a9015ae107443353ec85f488618a7080e5835bbe
Matthew Franglen <matthew@franglen.org>
2016-06-02 23:15:00 +0100

    Add demo of deserialization

--- ch05/Main.hs
+++ ch05/Main.hs
@@ -2,8 +2,10 @@ module Main (main) where
 
 import SimpleJSON (JValue(..))
 import Serializer (serialize)
+import Deserializer (deserialize)
 
 main = do
     let object = (JObject [("foo", JNumber 1), ("bar", JBool False), ("quxx", JArray [JString "towel", JNull])])
     putStrLn $ show object
     putStrLn $ serialize object
+    putStrLn $ show $ deserialize $ serialize object
