0b03fbaa0348725c3581b0f9bd52c18be51562d9
Matthew Franglen <matthew@franglen.org>
2016-06-16 20:39:29 +0100

    Add a demonstration of the use of second
    This is just a terrible question. The type signature for second is
    incomprehensible at this point.

--- ch06/Main.hs
+++ ch06/Main.hs
@@ -4,4 +4,12 @@ import Control.Arrow
 
 main :: IO ()
 main = do
-    putStrLn "hello"
+    putStrLn $ show $ secondDemo (+1) (1, 2)
+
+-- The second function takes a function and a tuple of two elements and applies
+-- the function to the second element.
+-- I have no idea how I was supposed to figure this out without the comments.
+
+-- secondDemo f (a, b) = (a, f b)
+secondDemo :: (b -> c) -> (a, b) -> (a, c)
+secondDemo f = second f
