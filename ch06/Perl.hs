55026ff4ee07547e6d43c59d7c39ea0080ad783a
Matthew Franglen <matthew@franglen.org>
2016-06-16 21:18:27 +0100

    Reasonably complete Black Perl in haskell
    This is a good way of seeing what is built into the language and what is a library.

--- ch06/Perl.hs
+++ ch06/Perl.hs
@@ -1,130 +1,131 @@
 import Prelude (undefined)
 
-poetry = do
-    BEFOREHAND: close door each window & exit; wait until time.
-        open spellbook study read (scan select tell us);
-    write it print the hex while each watches
-        reverse its length write again;
-        kill spiders pop them chop split kill them.
-            unlink arms shift wait & listen (listening wait)
-    sort the flock (_then warn the "goats" & kill the "sheep");
-        kill them dump qualms shift moralities
-        values aside each one;
+poetry = (
+    BEFOREHAND: close door, each window & exit. wait until time.
+        open spellbook, study, read (scan, select, tell us).
+    write it, print the hex while each watches,
+        reverse its length, write again.
+        kill spiders, pop them, chop, split, kill them.
+            unlink arms, shift, wait & listen (listening, wait),
+    sort the flock (_then, warn the "goats" & kill the "sheep").
+        kill them, dump qualms, shift moralities,
+        values aside, each one.
             die sheep! die to reverse the system
-            you accept (reject respect);
-    next step
-        kill the next sacrifice each sacrifice
-        wait redo ritual until "all the spirits are pleased";
+            you accept (reject, respect).
+    next step,
+        kill the next sacrifice, each sacrifice,
+        wait, redo ritual until "all the spirits are pleased".
         _do it ("as they say")
-    _do it(everyone***must***participate***_in***forbidden**s*e*x)
-    return last victim; package body;
-        exit crypt (time times & "half a time") & close it
-        select (quickly) & warn your next victim;
+    _do it(everyone***must***participate***_in***forbidden**s*e*x).
+    return last victim. package body.
+        exit crypt (time, times & "half a time") & close it,
+        select (quickly) & warn your next victim.
     AFTERWARDS: tell nobody.
-        wait wait until time;
-        wait until next year next decade;
-            sleep sleep die yourself
+        wait, wait until time.
+        wait until next year, next decade.
+            sleep, sleep, die yourself,
             die at last
+    )
 -- Larry Wall
 
 data BEFOREHAND = BEFOREHAND
 data AFTERWARDS = AFTERWARDS
 
 (***) = undefined
-(**) = undefined
-(*) = undefined
-(&) = undefined
-(!) = undefined
-(.) = undefined
+(**)  = undefined
+(*)   = undefined
+(&)   = undefined
+(!)   = undefined
+(.)   = undefined
 
-_do = undefined
-_in = undefined
-_then = undefined
-a = undefined
-accept = undefined
-again = undefined
-are = undefined
-arms = undefined
-aside = undefined
-at = undefined
-body = undefined
-chop = undefined
-close = undefined
-crypt = undefined
-decade = undefined
-die = undefined
-door = undefined
-dump = undefined
-e = undefined
-each = undefined
-everyone = undefined
-exit = undefined
-flock = undefined
-forbidden = undefined
-hex = undefined
-it = undefined
-its = undefined
-kill = undefined
-last = undefined
-length = undefined
-listen = undefined
-listening = undefined
-moralities = undefined
-must = undefined
-next = undefined
-nobody = undefined
-one = undefined
-open = undefined
-package = undefined
+_do         = undefined
+_in         = undefined
+_then       = undefined
+a           = undefined
+accept      = undefined
+again       = undefined
+are         = undefined
+arms        = undefined
+aside       = undefined
+at          = undefined
+body        = undefined
+chop        = undefined
+close       = undefined
+crypt       = undefined
+decade      = undefined
+die         = undefined
+door        = undefined
+dump        = undefined
+e           = undefined
+each        = undefined
+everyone    = undefined
+exit        = undefined
+flock       = undefined
+forbidden   = undefined
+hex         = undefined
+it          = undefined
+its         = undefined
+kill        = undefined
+last        = undefined
+length      = undefined
+listen      = undefined
+listening   = undefined
+moralities  = undefined
+must        = undefined
+next        = undefined
+nobody      = undefined
+one         = undefined
+open        = undefined
+package     = undefined
 participate = undefined
-pleased = undefined
-pop = undefined
-print = undefined
-qualms = undefined
-quickly = undefined
-read = undefined
-redo = undefined
-reject = undefined
-respect = undefined
-return = undefined
-reverse = undefined
-ritual = undefined
-s = undefined
-sacrifice = undefined
-say = undefined
-scan = undefined
-select = undefined
-sheep = undefined
-shift = undefined
-sleep = undefined
-sort = undefined
-spellbook = undefined
-spiders = undefined
-spirits = undefined
-split = undefined
-step = undefined
-study = undefined
-system = undefined
-tell = undefined
-the = undefined
-them = undefined
-they = undefined
-time = undefined
-times = undefined
-to = undefined
-unlink = undefined
-until = undefined
-us = undefined
-values = undefined
-victim = undefined
-wait = undefined
-warn = undefined
-watches = undefined
-while = undefined
-window = undefined
-write = undefined
-x = undefined
-year = undefined
-you = undefined
-your = undefined
-yourself = undefined
+pleased     = undefined
+pop         = undefined
+print       = undefined
+qualms      = undefined
+quickly     = undefined
+read        = undefined
+redo        = undefined
+reject      = undefined
+respect     = undefined
+return      = undefined
+reverse     = undefined
+ritual      = undefined
+s           = undefined
+sacrifice   = undefined
+say         = undefined
+scan        = undefined
+select      = undefined
+sheep       = undefined
+shift       = undefined
+sleep       = undefined
+sort        = undefined
+spellbook   = undefined
+spiders     = undefined
+spirits     = undefined
+split       = undefined
+step        = undefined
+study       = undefined
+system      = undefined
+tell        = undefined
+the         = undefined
+them        = undefined
+they        = undefined
+time        = undefined
+times       = undefined
+to          = undefined
+unlink      = undefined
+until       = undefined
+us          = undefined
+values      = undefined
+victim      = undefined
+wait        = undefined
+warn        = undefined
+watches     = undefined
+while       = undefined
+window      = undefined
+write       = undefined
+x           = undefined
+year        = undefined
+you         = undefined
+your        = undefined
+yourself    = undefined
