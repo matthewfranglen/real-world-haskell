f6b457266bc1c1a4ca8a3a36552e59d732d2a03a
Matthew Franglen <matthew@franglen.org>
2016-07-12 21:27:08 +0100

    Get example file reader compiling
    It seems to be broken

--- ch08/Main.hs
+++ ch08/Main.hs
@@ -1,7 +1,9 @@
-import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.ByteString.Lazy
 
 -- main = isElfFile "./Main.hs" >>= putStrLn . show
-main = putStrLn . show =<< isElfFile "/bin/bash"
+-- main = putStrLn . show =<< isElfFile "/bin/bash"
+main = putStrLn . show . readPrice =<< BS.readFile "./prices.csv"
 
 isElfFile :: FilePath -> IO Bool
 -- isElfFile path = BS.readFile path >>= return . hasElfMagic
@@ -10,17 +12,17 @@ isElfFile path = hasElfMagic <$> BS.readFile path
 
 hasElfMagic :: BS.ByteString -> Bool
 hasElfMagic content = BS.take 4 content == elfMagic
-    where elfMagic = BS.pack [0x7f, 0x45, 0x4c, 0x46]
+    where elfMagic = Data.ByteString.Lazy.pack [0x7f, 0x45, 0x4c, 0x46]
 
 closingPrice :: BS.ByteString -> Maybe Int
 closingPrice = readPrice . (!!4) . BS.split ','
 
 readPrice :: BS.ByteString -> Maybe Int
 readPrice str =
-    case L.readInt str of
+    case BS.readInt str of
       Nothing             -> Nothing
       Just (dollars,rest) ->
-        case L.readInt (L.tail rest) of
+        case BS.readInt (BS.tail rest) of
           Nothing           -> Nothing
           Just (cents,more) ->
             Just (dollars * 100 + cents)
