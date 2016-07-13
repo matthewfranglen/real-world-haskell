fb40218bc9a4ce42d119940902d453ac2eb5a2e1
Matthew Franglen <matthew@franglen.org>
2016-07-13 21:03:55 +0100

    Really copy

--- ch08/src/Regexy.hs
+++ ch08/src/Regexy.hs
@@ -19,3 +19,13 @@ globToRegex' ('[':c:cs)     = '['  :  c : charClass cs
 globToRegex' ('[':_)        = error "unterminated character class"
 
 globToRegex' (c:cs) = escape c ++ globToRegex' cs
+
+escape :: Char -> String
+escape c | c `elem` regexChars = '\\' : [c]
+         | otherwise = [c]
+    where regexChars = "\\+()^$.{}]|"
+
+charClass :: String -> String
+charClass (']':cs) = ']' : globToRegex' cs
+charClass (c:cs)   = c : charClass cs
+charClass []       = error "unterminated character class"
