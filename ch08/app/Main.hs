250b9a319b07e376ed5fea4f097559f54519fd89
Matthew Franglen <matthew@franglen.org>
2016-07-13 20:41:57 +0100

    Start on regex stuff

--- ch08/app/Main.hs
+++ ch08/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.ByteString.Lazy.Char8 as BS
 import Closy
 import Elfy
+import Regexy
 
 -- main = isElfFile "./Main.hs" >>= putStrLn . show
 -- main = putStrLn . show =<< isElfFile "/bin/bash"
