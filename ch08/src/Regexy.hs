3609a541f691cf205403099d3d77fd9017c42138
Matthew Franglen <matthew@franglen.org>
2016-07-13 21:02:30 +0100

    Copy over globToRegex

--- ch08/src/Regexy.hs
+++ ch08/src/Regexy.hs
@@ -3,3 +3,19 @@ module Regexy
     ) where
 
 import Text.Regex.Posix
+
+globToRegex :: String -> String
+globToRegex cs = '^' : globToRegex' cs ++ "$"
+
+globToRegex' :: String -> String
+globToRegex' "" = ""
+
+globToRegex' ('*':cs) = ".*" ++ globToRegex' cs
+
+globToRegex' ('?':cs) = '.' : globToRegex' cs
+
+globToRegex' ('[':'!':c:cs) = "[^" ++ c : charClass cs
+globToRegex' ('[':c:cs)     = '['  :  c : charClass cs
+globToRegex' ('[':_)        = error "unterminated character class"
+
+globToRegex' (c:cs) = escape c ++ globToRegex' cs
